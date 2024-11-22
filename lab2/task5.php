f716dd1be193ddbf78ef9b06a8ebac718e9f849e
zafir-ken <zafirkenzaman9806@gmail.com>
2024-11-22 15:33:08 +0600

    Create task5.php

---
+++ lab2/task5.php
@@ -0,0 +1,13 @@
+<?php
+    for ($i = 10; $i <= 100; $i++) 
+    {
+        if ($i % 2 != 0) 
+        {
+            echo $i . "<br>";
+        } 
+        else 
+        {
+            continue;
+        }
+    }
+?>
