ce2fcab9c5c918195a7890e3429788dd89c6dcdd
zafir-ken <zafirkenzaman9806@gmail.com>
2024-11-24 13:05:40 +0600

    Create home.php

---
+++ mid_lab_exam/home.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if(isset($_POST['logout']))
+{
+    session_destroy();
+    header("Location: login.php");
+}
+     
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <th>WELCOME <?php
+         echo "  " .  $_SESSION['name'] ;
+         ?></th>
+    </table>
+    <form action="" method="POST">
+        <button type="submit" name="logout">LOGOUT</button>
+    </form>
+</body>
+</html>
