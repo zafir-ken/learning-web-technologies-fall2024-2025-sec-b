cf294f38c5efc78218e3f0da1656310784fd4243
zafir-ken <zafirkenzaman9806@gmail.com>
2024-11-24 13:32:10 +0600

    Update logincheck.php

--- mid_lab_exam/logincheck.php
+++ mid_lab_exam/logincheck.php
@@ -2,10 +2,13 @@
 
 session_start();
 
+$_SESSION['flag']="0";
+
 if($_SESSION["Lemail"]==$_SESSION['email'] )
 {
     if($_SESSION["Lpassword"]==$_SESSION['password'])
     {
+        $_SESSION['flag']="1";
         header("Location: home.php");
         //echo "milse";
        
